257b98c8b4ae8eac69f64c1f6c1b96e25129d352
alsdn2025 <alsdn202530@gmail.com>
2022-11-15 20:32:46 +0900

    Git Merge, Add WP inference function

---
+++ app/src/main/java/com/example/myapp1/ClassifierWithTFLiteSupport.java
@@ -0,0 +1,165 @@
+package com.example.myapp1;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import org.tensorflow.lite.Tensor;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.model.Model;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class ClassifierWithTFLiteSupport {
+    private final static String TAG = "ClassifierWithTFLiteSupport"; // for logging
+    private final String MODEL_NAME;
+    private final String LABEL_FILE;
+
+    int modelInputWidth, modelInputHeight, modelInputChannel;
+    Context context;
+    TensorImage inputImage; // 이미지 관련 데이터 저장용 인스턴스
+    TensorBuffer outputBuffer; // 출력 결과를 담을 버퍼
+    private List<String> labels; // 라벨 정보를 담을 리스트, assets의 txt 파일에서 read
+
+    Model model; // tflite 로드부터 추론까지 모두 수행 가능한 model 개체
+
+    // context 만 전달시 테스트라고 간주, 1_to_30 모델&라벨 파일 설정
+    public ClassifierWithTFLiteSupport(Context context){
+        this.context = context;
+        MODEL_NAME = "MobileNetV2_leaf(front)_1_to_30.tflite";
+        LABEL_FILE = "labels_leaf_1_to_30.txt";
+    }
+
+    // 객체 생성시 전달받은 식물 부위(기관)에 따라 사용할 모델 및 라벨 파일 설정
+    public ClassifierWithTFLiteSupport(Context context, PlantOrgans organ){
+        this.context = context;
+        switch (organ){
+            case FLOWER:
+                MODEL_NAME = "Flowers_ResNet50.tflite";
+                LABEL_FILE = "labels_Flower.txt";
+                break;
+            case FRUIT:
+                MODEL_NAME = "Fruits_ResNet50.tflite";
+                LABEL_FILE = "labels_Fruit.txt";
+                break;
+            case LEAF:
+                MODEL_NAME = "Leaf(front)_ResNet50.tflite";
+                LABEL_FILE = "labels_leaf.txt";
+                break;
+            default:
+                Log.e(TAG, "생성자 파라미터가 적합하지 않음, default 값으로 세팅");
+                MODEL_NAME = "MobileNetV2_leaf(front)_1_to_30.tflite";
+                LABEL_FILE = "labels_leaf_1_to_30.txt";
+        }
+    }
+
+    public ClassifierWithTFLiteSupport(Context context, String modelName, String labelFile){
+        this.context = context;
+        MODEL_NAME = modelName;
+        LABEL_FILE = labelFile;
+    }
+
+    // modelInputShape 멤버변수를 초기화하는 메서드, init()에서 호출됨
+    public void initModelShape(){
+        Tensor inputTensor = model.getInputTensor(0);
+        Tensor outputTensor = model.getOutputTensor(0);
+        int[] inputShape = inputTensor.shape();
+        modelInputChannel = inputShape[0];
+        modelInputWidth = inputShape[1];
+        modelInputHeight = inputShape[2];
+
+        inputImage = new TensorImage(inputTensor.dataType()); // Float32 타입으로 TensorImage 개체 생성
+        // 모델의 출력 텐서의 shape, data type 에 맞게 고정된 사이즈의 output 버퍼 개체 생성
+        outputBuffer = TensorBuffer.createFixedSize(outputTensor.shape(), outputTensor.dataType());
+    }
+
+    // 생성자와 별도로 model 등 필드를 초기화하는 메서드
+    public void init() throws IOException {
+        model = Model.createModel(context, MODEL_NAME);
+        Log.e(TAG, "model created");
+
+        initModelShape();
+        labels = FileUtil.loadLabels(context, LABEL_FILE); // label 텍스트 파일 읽고 저장
+    }
+
+    // Bitmap 이미지를 입력받고, 이를 전처리하고 TensorImage 개체로 리턴하는 메서드, classify() 에서 호출됨
+    private TensorImage loadImage(final Bitmap bitmap){
+        // Tensor Image 는 ARGB_8888 포맷의 Bitmap 만을 받음, 따라서 확인 및 변환 필요
+        if(bitmap.getConfig() != Bitmap.Config.ARGB_8888){
+            inputImage.load(bitmap.copy(Bitmap.Config.ARGB_8888, true));
+        }else {
+            inputImage.load(bitmap);
+        }
+
+        // Image 전처리 과정, resizing, normalizing 등이 필요하면 여기에 작성
+        // Resizing은 최근접 보간법(Nearest neighbor interpolation) 사용
+        ImageProcessor imageProcessor = new ImageProcessor.Builder()
+                .add(new ResizeOp(
+                        modelInputHeight,
+                        modelInputWidth,
+                        ResizeOp.ResizeMethod.NEAREST_NEIGHBOR)
+                ).build();
+
+        return imageProcessor.process(inputImage);
+    }
+
+
+    // classify()에서 호출
+    // 모델로부터 받은 Output 맵의 value 를 정규화하고, value 를 기준으로 정렬된 Entry List 로 만들어 리턴
+    private List<Entry<String, Float>> getSortedEntryList(Map<String, Float> map){
+        float sumVal = 0;
+
+        List<Entry<String, Float>> list_entries = new ArrayList<>(map.entrySet());
+        Collections.sort(list_entries, (obj1, obj2) -> {
+            // 내림 차순 정렬
+            return obj2.getValue().compareTo(obj1.getValue());
+        });
+
+        for (Entry<String, Float> entry: list_entries){
+            // logging, 잘 보이게 Error 로 표시
+            String str = "String:" + entry.getKey() + " Val: " + entry.getValue();
+            Log.e(TAG, str);
+
+            sumVal += entry.getValue();
+        }
+
+        for (Entry<String, Float> entry: list_entries){
+            entry.setValue(entry.getValue()/sumVal);
+        }
+
+        return list_entries;
+    }
+
+    // 추론 메서드.
+    public List<Entry<String, Float>> classify(Bitmap image){
+        inputImage = loadImage(image);
+
+        // outputBuffer 는 rewind 해서 전달
+        Object[] inputs = new Object[]{inputImage.getBuffer()};
+        Map<Integer, Object> outputs = new HashMap<>();
+        outputs.put(0, outputBuffer.getBuffer().rewind());
+
+        // 추론
+        model.run(inputs, outputs);
+
+        // 모델이 추론한 결과와 label 리스트를 매핑
+        Map<String, Float> output = new TensorLabel(labels, outputBuffer).getMapWithFloatValue();
+
+        return getSortedEntryList(output);
+    }
+
+    public void finish(){
+        if(model != null)
+            model.close();
+    }
+}
---
+++ app/src/main/java/com/example/myapp1/InputDataActivity.java
@@ -0,0 +1,173 @@
+package com.example.myapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/*
+ * usage : 갤러리에서 사진을 꺼내어 추론 요청, 결과 표시
+ * */
+public class InputDataActivity extends AppCompatActivity {
+    public static final int GALLERY_IMAGE_REQUEST_CODE = 1;
+    private static final String TAG = "InputDataActivity";
+    private ImageView imageView;
+    private TextView output_textView;
+    private PlantOrgans organ;
+    private ClassifierWithTFLiteSupport classifier;
+    private List<Map.Entry<String, Float>> outputList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_data);
+
+        Button selectBtn = findViewById(R.id.selectBtn);
+        selectBtn.setOnClickListener(view -> getImageFromGallery());
+
+        Button detailBtn = findViewById(R.id.detailBtn);
+        detailBtn.setOnClickListener(view -> getDetailedOutput());
+
+        imageView = findViewById(R.id.imageView);
+        output_textView = findViewById(R.id.output_textView);
+        TextView organ_textView = findViewById(R.id.organ_textView);
+
+        Intent intent = getIntent();
+        organ = (PlantOrgans) intent.getSerializableExtra("organ");
+        if(organ == null){
+            Log.e(TAG, "Organ is empty, Create classifier with default constructor");
+        }
+        setOrganTextView(organ_textView);
+
+
+        classifier = new ClassifierWithTFLiteSupport(this, organ);
+        try{
+            classifier.init();
+        }catch (IOException e){
+            Log.e(TAG,"Classifier initiate error");
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setOrganTextView(TextView textView){
+        switch (this.organ){
+            case FLOWER:
+                textView.setText("Flower");
+                break;
+            case FRUIT:
+                textView.setText("Fruit");
+                break;
+            case LEAF:
+                textView.setText("Leaf");
+                break;
+            default:
+                textView.setText("Leaf(1_to_30)");
+        }
+    }
+
+    private void getImageFromGallery(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT).setType("image/*");
+        try{
+            startActivityForResult(intent, GALLERY_IMAGE_REQUEST_CODE); // deprecated
+        }catch (Exception e){
+            Log.e(TAG, "Failed to get Image from Gallery", e);
+        }
+    }
+
+    // 추론 결과로 받은 List<Entry>를 하나의 String 으로 변환하여 Intent 에 전달,
+    // Detailed Output Activity 실행
+    private void getDetailedOutput(){
+        try{
+            Intent i = new Intent(InputDataActivity.this, detailedOutputActivity.class);
+            StringBuilder sb = new StringBuilder();
+            for (Map.Entry<String, Float> entry: outputList){
+                sb.append(entry.getKey()).append(" : ").append(entry.getValue()).append(System.lineSeparator());
+            }
+            i.putExtra("output", sb.toString());
+            i.putExtra("fileName", ((TextView)findViewById(R.id.filename_textview)).getText());
+            startActivity(i);
+        }catch (Exception e){
+            Log.e(TAG, "Failed to ", e);
+        }
+    }
+
+    // uri 객체로부터 파일 이름을 가져와 filename_text 뷰에 setText 하는 메서드
+    // https://developer.android.com/training/secure-file-sharing/retrieve-info?hl=ko#java
+    @SuppressLint("SetTextI18n")
+    private void displayImageNameFromUri(Uri uri){
+        /*
+         * Get the file's content URI from the incoming Intent,
+         * then query the server app to get the file's display name
+         * and size.
+         */
+        Cursor returnCursor =
+                getContentResolver().query(uri, null, null, null, null);
+        /*
+         * Get the column indexes of the data in the Cursor,
+         * move to the first row in the Cursor, get the data,
+         * and display it.
+         */
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        returnCursor.moveToFirst();
+        TextView filename_textView = findViewById(R.id.filename_textview);
+
+        filename_textView.setText("File name : " + returnCursor.getString(nameIndex));
+        returnCursor.close();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK && requestCode == GALLERY_IMAGE_REQUEST_CODE){
+            if(data == null)
+                return;
+
+            Uri selectedImage = data.getData();
+            displayImageNameFromUri(selectedImage);
+            Bitmap bitmap = null;
+
+            try{
+                if(Build.VERSION.SDK_INT >= 29){
+                    ImageDecoder.Source src =
+                            ImageDecoder.createSource(getContentResolver(), selectedImage);
+                    bitmap = ImageDecoder.decodeBitmap(src);
+                } else{
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                }
+            }catch (IOException e ){
+                Log.e(TAG, "Failed to read Image", e);
+            }
+
+            if(bitmap != null){
+                outputList = classifier.classify(bitmap);
+                String resultStr = String.format(Locale.ENGLISH,
+                        "Inferred class : %s " + System.lineSeparator() + "Probability : %.2f%%",
+                        outputList.get(0).getKey(), outputList.get(0).getValue() * 100);
+
+                imageView.setImageBitmap(bitmap);
+                output_textView.setText(resultStr);
+            }else{
+                Log.e(TAG,"Bitmap is null Object");
+            }
+        }
+    }
+}
